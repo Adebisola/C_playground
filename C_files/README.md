905fcba2f6f129dd1e26dcc85db2d15619e65d42
Adebisola <adeniyiadebisola@gmail.com>
2022-09-11 09:21:02 -0700

    Initial commit

---
+++ C_files/README.md
@@ -0,0 +1,1 @@
+Hello.c:I prepend ./ to the program name to tell the terminal that the command is in the current folder.
---
+++ C_files/set_Variable.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	/*local variable declaration*/
+	int age = 0;
+	age = 38.2;
+	printf("%u" , age);
+}
