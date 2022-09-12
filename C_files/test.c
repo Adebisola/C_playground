f824a4bc8a4338c83e82b6b8d95609fbb7c8e651
Adebisola <adeniyiadebisola@gmail.com>
2022-09-12 03:40:25 -0700

    practice

---
+++ C_files/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	unsigned char x = 255;
+	x = x+10;
+	printf("%u\n" , x);
+char j = 127;
+j = j+10;
+printf("%u\n" , j);
+}
+
---
+++ C_files/test0.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a = 4;
+	a = a++;
+	printf("%u\n" , a);
+	int b = 6;
+	b = ++b;
+	printf("%u\n" , b);
+	
+	int x = 2;
+	y = x++;
+	y = ++x;
+	printf("%u\n" , x);
+	printf("%u\n" , y):
+
+}
