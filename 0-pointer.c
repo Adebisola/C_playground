4215837d6b045a02c0352c3bcf2f56e23400456f
Adebisola <adeniyiadebisola@gmail.com>
2022-09-24 08:35:02 -0700

    pointers

---
+++ 0-pointer.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{
+	int x = 1;
+	printf ("x is stored at %p in memory\n", &x);
+}
---
+++ switcheroo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+switch (train) {
+		case 37:
+
+			winnings = winnings + 50;
+			
+			break;
+		case 65:
+			
+			puts ("Jackpot!");
+			
+			winnings = winnings + 80;
+		case 12:
+
+			
+			winnings = winnings + 20;
+			
+			break;
+		default:
+
+			winnings = 0;
+	}
