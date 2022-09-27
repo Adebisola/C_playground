84463960d90c7450b598475397113ba0cecb2203
Adebisola <adeniyiadebisola@gmail.com>
2022-09-27 09:40:18 -0700

    more on pointers

--- headfastc/func-arg_str.c
+++ headfastc/func-arg_str.c
@@ -2,13 +2,15 @@
 
 void print_msg(char msg[])
 {
-	printf("Message reads: %s\n", msg);
+	printf ("Message reads: %s\n", msg);
+	printf ("msg occupies %lu bytes in memory\n", sizeof (msg));
 }
 
 int main()
 {
 	char quote[] = "Cookies make you fat";
 	print_msg(quote);
+	printf ("The quote string is stored at %p\n", quote);
 	
 	return (0);
 }
