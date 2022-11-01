afcf0c0352b3cc76196f41e67ec7b6445d89b528
Adebisola <adeniyiadebisola@gmail.com>
2022-11-01 13:45:16 -0700

    scanf

---
+++ headfastc/scanf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ * Description - main funtion uses pointers for data entry.
+ * Return : inputed string.
+ */
+
+
+int main()
+{
+	char name[40];
+
+	printf ("Enter your name: ");
+
+	scanf ("%39s", name);
+}
