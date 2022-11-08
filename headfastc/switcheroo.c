8db55589ad78c79de25945e13a7728bcb1f25b1b
Adebisola <adeniyiadebisola@gmail.com>
2022-11-08 07:46:17 -0800

    moved files to headfastc from C-playground

---
+++ headfastc/switcheroo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	switch (train) {
+                case 37:
+
+                        winnings = winnings + 50;
+
+                        break;
+                case 65:
+
+                        puts ("Jackpot!");
+
+                        winnings = winnings + 80;
+                case 12:
+
+
+                        winnings = winnings + 20;
+
+                        break;
+                default:
+
+                        winnings = 0;
+	}
+}
